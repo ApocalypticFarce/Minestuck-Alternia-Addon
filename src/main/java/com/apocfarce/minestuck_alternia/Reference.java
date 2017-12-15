6e07934f5c22dcd6e69d1037d23c7ac260d145dc
seriouscoolness <wyattrobert@gmail.com>
2017-12-14 22:52:43 -0500

    Committed mod project files.

---
+++ src/main/java/com/apocfarce/minestuck_alternia/Reference.java
@@ -0,0 +1,11 @@
+package com.apocfarce.minestuck_alternia;
+
+public class Reference {
+
+	public static final String MOD_ID = "minestuck_alternia";
+	public static final String NAME = "Minestuck Alternia Addon";
+	public static final String VERSION = "0.1";
+	
+	public static final String CLIENT_PROXY_CLASS = "com.apocfarce.minestuck_alternia.proxy.ClientProxy";
+	public static final String SERVER_PROXY_CLASS = "com.apocfarce.minestuck_alternia.proxy.ServerProxy";
+}
